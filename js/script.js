7a2c885a49b5735e0f43f919f7a50f2115d8ac60
Nebil ferej <nabilrooney123@gmail.com>
2025-04-19 23:19:46 -0700

    Add fade-in

--- js/script.js
+++ js/script.js
@@ -143,5 +143,18 @@ document.getElementById("darkModeToggle").addEventListener("click", function () 
     document.body.classList.toggle("dark-mode");
   });
   
+  const fadeElements = document.querySelectorAll(".fade-in");
 
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  }, {
+    threshold: 0.2
+  });
+  
+  fadeElements.forEach((el) => observer.observe(el));
+  
 
